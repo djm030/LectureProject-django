930d3ab16999cd22ab4859d31bbfcdeabbaf92cf
백관열 <djm030@naver.com>
2023-03-08 21:03:38 +0900

    0308

--- lectures/serializers.py
+++ lectures/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Lecture, calculatedLecture
+from videos.serializers import VideoSerializer
 
 
 class LectureListSerializer(serializers.ModelSerializer):
@@ -15,7 +16,17 @@ class LectureListSerializer(serializers.ModelSerializer):
 
 class LectureDetailSerializer(serializers.ModelSerializer):
     lecture = LectureListSerializer(read_only=True)
+    # totalVideoLength = serializers.SerializerMethodField()
+    # video = VideoSerializer()
 
     class Meta:
         model = calculatedLecture
         fields = "__all__"
+
+    # def get_totalVideoLength(self, obj):
+    #     video_lengths = [video.length for video in obj.videos.all()]
+    #     print(video_lengths)
+    #     total_length = sum(video_lengths)
+    #     total_length_in_seconds = total_length.total_seconds()
+
+    #     return total_length_in_seconds
