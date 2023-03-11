82a52b3ec2dc8c3e0a079799359ce74de115d216
lee seung ah <lee88067@gmail.com>
2023-03-11 23:38:57 +0900

    0311

--- categories/admin.py
+++ categories/admin.py
@@ -4,4 +4,8 @@ from .models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("classification", "name", "order",)
+    list_display = (
+        "classification",
+        "name",
+        "order",
+    )
