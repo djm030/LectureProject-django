381fad07afc62ac099bde2b3f89d43f010756b85
백관열 <djm030@naver.com>
2023-03-13 17:57:55 +0900

    0313 commit

--- cart/models.py
+++ cart/models.py
@@ -7,6 +7,7 @@ class Cart(CommonModel):
         "lectures.Lecture",
         on_delete=models.CASCADE,
         related_name="cart",
+        null=True,
     )
     rating = models.CharField(max_length=10)
 
