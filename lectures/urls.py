5b6d4437fada6a2ede49e7408413c261730998c8
백관열 <djm030@naver.com>
2023-03-16 11:21:50 +0900

    change lectures

--- lectures/urls.py
+++ lectures/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.Lectures.as_view()),
     path("<int:pk>", views.LecturesDetail.as_view()),
-    path("<str:username>", views.InstructorName.as_view()),
-
+    path("search", views.SearchLectures.as_view()),
+    path("<str:category1>", views.OneCategory.as_view()),
+    path("<str:category1>/<int:pages>", views.OneCategoryPage.as_view()),
+    path("<str:category1>/<str:category2>", views.TwoCategory.as_view()),
+    path(
+        "<str:category1>/<str:category2>/<int:pages>", views.TwoCategoryPage.as_view()
+    ),
+    path("@<str:username>", views.InstructorName.as_view()),
 ]
--- lectures/views.py
+++ lectures/views.py
@@ -3,19 +3,20 @@ from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, ParseError
 from .models import Lecture
-from users.models import User
-from reviews.models import Review
 from . import serializers
 from rest_framework import permissions
-
-# 전체 게시글을 불러오는 API
+from categories.models import Category
+from categories.serializers import CategorySerializer
+from django.conf import settings
+from users.models import User
 
 
 class Lectures(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         try:
             page = request.query_params.get("page", 1)
-
             page = int(page)
         except ValueError:
             page = 1
@@ -23,11 +24,12 @@ class Lectures(APIView):
         page_size = 30
         start = (page - 1) * page_size
         end = start + page_size
-
+        total_num = Lecture.objects.count()
+        print(total_num)
         serializer = serializers.LectureSerializer(
             Lecture.objects.all()[start:end], many=True
         )
-        return Response(serializer.data)
+        return Response({"data": serializer.data, "totalNum": total_num})
 
     def post(self, request):
         if request.user.isInstructor:
@@ -42,10 +44,9 @@ class Lectures(APIView):
             raise ParseError()
 
 
-# 하나의 게시글을 불러오는 API
-
-
 class LecturesDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Lecture.objects.get(pk=pk)
@@ -78,7 +79,101 @@ class LecturesDetail(APIView):
         return Response(status=HTTP_204_NO_CONTENT)
 
 
-# 강사이름 기반으로 특정 게시글을 불러오는 API
+class SearchLectures(APIView):
+    def get(self, request):
+        # Filter by search words
+        search_words = request.query_params.get("search")
+        if search_words:
+            lectures = Lecture.objects.filter(lectureTitle__icontains=search_words)
+        else:
+            lectures = Lecture.objects.all()
+        # Apply category filter if specified
+        # Paginate results
+        total_num = lectures.count()
+        page_size = 30
+        page = int(request.query_params.get("page", 1))
+        start = (page - 1) * page_size
+        end = start + page_size
+        paged_lectures = lectures[start:end]
+        # Serialize results
+        serializer = serializers.LectureSerializer(paged_lectures, many=True)
+        return Response({"data": serializer.data, "totalNum": total_num})
+
+
+class OneCategory(APIView):
+    def get_CategoryObject(self, category1):
+        try:
+            return Category.objects.get(name=category1)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, category1):
+        category = self.get_CategoryObject(category1)
+        lectures = Lecture.objects.filter(categories=category)
+        total_num = lectures.count()
+        page_size = 30
+        page = int(request.query_params.get("page", 1))
+        start = (page - 1) * page_size
+        end = start + page_size
+        paged_lectures = lectures[start:end]
+        # Serialize results
+        serializer = serializers.LectureSerializer(paged_lectures, many=True)
+        return Response({"data": serializer.data, "totalNum": total_num})
+
+
+class OneCategoryPage(APIView):
+    def get_CategoryObject(self, category1):
+        try:
+            return Category.objects.get(name=category1)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, category1, pages):
+        category = self.get_CategoryObject(category1)
+        lectures = Lecture.objects.filter(categories=category)
+        total_num = lectures.count()
+        page_size = 30
+        start = (pages - 1) * page_size
+        end = start + page_size
+        paged_lectures = lectures[start:end]
+        # Serialize results
+        serializer = serializers.LectureSerializer(paged_lectures, many=True)
+        return Response({"data": serializer.data, "totalNum": total_num})
+
+
+class TwoCategory(APIView):
+    def get(self, request, category1, category2):
+        category_1 = OneCategoryPage().get_CategoryObject(category1)
+        category_2 = OneCategoryPage().get_CategoryObject(category2)
+        lectures = Lecture.objects.filter(
+            categories__in=[category_1, category_2]
+        ).distinct()
+        total_num = lectures.count()
+        page_size = 30
+        page = int(request.query_params.get("page", 1))
+        start = (page - 1) * page_size
+        end = start + page_size
+        paged_lectures = lectures[start:end]
+        # Serialize results
+        serializer = serializers.LectureSerializer(paged_lectures, many=True)
+        return Response({"data": serializer.data, "totalNum": total_num})
+
+
+class TwoCategoryPage(APIView):
+    def get(self, request, category1, category2, page):
+        category_1 = OneCategoryPage().get_CategoryObject(category1)
+        category_2 = OneCategoryPage().get_CategoryObject(category2)
+        lectures = Lecture.objects.filter(
+            categories__in=[category_1, category_2]
+        ).distinct()
+        total_num = lectures.count()
+        page_size = 30
+        start = (page - 1) * page_size
+        end = start + page_size
+        paged_lectures = lectures[start:end]
+        # Serialize results
+        serializer = serializers.LectureSerializer(paged_lectures, many=True)
+        return Response({"data": serializer.data, "totalNum": total_num})
 
 
 class InstructorName(APIView):
